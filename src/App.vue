4fda197591bb2c212e936ffe768d1c9a249099b9
zhongqiulan <994700551@qq.com>
2018-05-28 07:56:16 +0800

    vue 实现的小米官网首页

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <!-- <button @click="showModal = true">Show Modal</button> -->
+    <!-- <modal-template v-if="showModal" @close="showModal = false"></modal-template> -->
+    <top-bar></top-bar>
+    <top-header></top-header>
+    <top-banner></top-banner>
+    <top-sub></top-sub>
+    <goods></goods>
+    <temp-footer ></temp-footer>
+    <play-video :play-config="playConfig"></play-video>
+  </div>
+</template>
+
+<script>
+import TopBar from './components/TopBar'
+import TopHeader from './components/TopHeader'
+import TopBanner from './components/TopBanner'
+import TopSub from './components/TopSub'
+import Goods from './components/Goods'
+import TempFooter from './components/TempFooter'
+import PlayVideo from './components/common/PlayVideo'
+import ModalTemplate from './components/modalTemplate'
+export default {
+  data () {
+    return {
+      playConfig: {},
+      showModal: false
+    }
+  },
+  ready () {
+    this.recivePlayVideo()
+  },
+  methods: {
+    recivePlayVideo () {
+      this.$on('play', function (opts) {
+        this.playConfig = opts
+      })
+    }
+  },
+  components: {
+    'top-bar': TopBar,
+    'top-header': TopHeader,
+    'top-banner': TopBanner,
+    'top-sub': TopSub,
+    'goods': Goods,
+    'temp-footer': TempFooter,
+    'play-video': PlayVideo,
+    'modal-template': ModalTemplate
+  }
+}
+</script>
+
+<style>
+@import './assets/css/base.css';
+</style>
