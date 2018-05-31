fc6f7a5c5f87b13a43b5b8575293e8a8c7a806d2
zhongqiulan <994700551@qq.com>
2018-05-31 22:07:23 +0800

    引入了vue-ui框架

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import 'normalize.css'
 import 'font-awesome/css/font-awesome.min.css'
 import Vue from 'vue'
 import App from './App'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
